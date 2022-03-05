b843743602900f1fb6645b9c11950b6aead6fe99
Vaghinak <arrat.armenia.02@mail.ru>
2022-03-05 09:59:50 -0800

    Registration1fdasasdfdsfasdfdsfasd

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -13,7 +13,7 @@ export const sendForm = e =>
             if(res.data.ok)
             {
                 setNext(true);
-                setError("d")
+                setError("")
             }
             else if(res.status === 200)
             {
