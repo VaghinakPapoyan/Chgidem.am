50b976cb994ec3fa1554d6442d5ce6f79def8ade
Rud <rudikhayk2017@gmail.com>
2022-03-05 20:04:16 +0300

    Update Header.jsx

--- client/src/Components/Main-Components/Header.jsx
+++ client/src/Components/Main-Components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { Link } from "react-router-dom"
 
 const HeaderComponent = styled.div`
