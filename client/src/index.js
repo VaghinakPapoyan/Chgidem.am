8943a31ab9ce8d16639ea777f431a732636854e1
Rud <rudikhayk2017@gmail.com>
2022-03-05 11:49:32 +0300

    global styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin:0px;
+    padding:0px;
+  }
+`
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
