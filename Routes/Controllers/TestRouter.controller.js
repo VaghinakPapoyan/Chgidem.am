f45537ff1eb60c4b0395c2d5edc5607a36addce6
Vaghinak Papoyan <50072682+Anonimus1423@users.noreply.github.com>
2022-03-06 03:53:22 -0800

    add use Text

--- Routes/Controllers/TestRouter.controller.js
+++ Routes/Controllers/TestRouter.controller.js
@@ -1,12 +1,12 @@
-import {Test} from "../../Models/Test.js";
+import { Test } from "../../Models/Test.js";
 
 export async function Add(req,res){
     try{
-        const {title} = req.body
+        const { title } = req.body
         const newTusk =  new Test({title})
         await newTusk.save()
         return res.json({
-            message:newTusk
+            message: newTusk
         })
     }catch(e){
         res.json({
