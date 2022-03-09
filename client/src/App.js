6ad5e0250995c69dbe999140b4fe1445f5514f61
Rud <rudikhayk2017@gmail.com>
2022-03-09 20:31:54 +0300

    a

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { loading } from "./Components/loading";
+import route from "./Components/Route";
+import { useEffect, useState } from "react"
+
+function App() {
+  const token = useSelector(state=>state.token)
+  const user = useSelector(state=>state.user)
+  const dispatch = useDispatch()
+  let [bool, setBool] = useState(false);
+  useEffect(()=>{
+    if(token !== null){
+      loading(dispatch,token).then(res=>{setBool(res)})
+    }    
+    setBool(false)
+  },[token])
+  const router = route(bool)
+
+  return (
+    <BrowserRouter>
+      {router}
+    </BrowserRouter>
+    );
+}
+
+export default App;
